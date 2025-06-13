f30e68cfb0e1f1fb2706e00b0d634fc2517a5bb8
Mike Malkhasyan <mike.malkhasyan@vertexinc.com>
2025-06-13 21:44:20 +0400

    - configure swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
+  const port = process.env.APP_HOST ?? 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Skeleton API')
+    .setDescription(
+      "API helps to load data from external API's and build aggregated result",
+    )
+    .setVersion('1.0')
+    .addServer(String(port))
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/documentation', app, documentFactory);
+
+  await app.listen(port);
 }
 bootstrap();
