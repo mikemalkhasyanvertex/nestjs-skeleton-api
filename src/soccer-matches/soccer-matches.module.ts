b60d2daabf9cb32d279f9e71534633e06ace77b9
Mike Malkhasyan <mike.malkhasyan@vertexinc.com>
2025-06-15 12:13:51 +0400

    - add soccer matches module

--- src/soccer-matches/soccer-matches.module.ts
+++ src/soccer-matches/soccer-matches.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { SoccerMatchesService } from './soccer-matches.service';
 import { SoccerMatchesController } from './soccer-matches.controller';
-import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule],
   controllers: [SoccerMatchesController],
   providers: [SoccerMatchesService],
 })
